a4c8b8a63ff5a554c949a934d1012842e12450cf
thotluna <eladi.feijoo@gmail.com>
2021-02-24 00:25:38 -0400

    create game state

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyledDisplay } from "./StyledDisplay";
 
-export default function Display({ gameOver, text }) {
-  return <StyledDisplay gameOver={gameOver}>{text}</StyledDisplay>;
+export default function Display({ alert, text }) {
+  return <StyledDisplay alert={alert}>{text}</StyledDisplay>;
 }
---
+++ src/types.js
@@ -0,0 +1,6 @@
+export const stateGame = {
+  OFF: "off",
+  IN_GAME: "in-game",
+  PAUSE: "pause",
+  GAME_OVER: "game-over",
+};
